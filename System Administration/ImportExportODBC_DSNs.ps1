542599253ba42ef26e9c234e3c4d29db5f791c9c
Fany Vargas <14260028+fvarga01@users.noreply.github.com>
2020-04-28 03:25:07 -0500

    Added reg import/export

--- System Administration/ImportExportODBC_DSNs.ps1
+++ System Administration/ImportExportODBC_DSNs.ps1
@@ -7,25 +7,26 @@
 #**************************************************************************************************************
 
 write-host "******************** List ODBC Drivers ****************************************"
-Get-OdbcDriver -Platform 'All' | foreach {
-    $attr = $_ | select -ExpandProperty Attribute
-    New-Object -TypeName PSObject -Property @{
-        ComputerName = $env:COMPUTERNAME
-        Name = $_.Name
-        Platform = $_.Platform
-        Driver  =  $attr["Driver"]
-        DriverODBCVer  =  $attr["DriverODBCVer"]
-        } 
-    } | select Computername, Name,Platform, DriverODBCVer,Driver  | ft
-
+Get-OdbcDriver -Platform 'All' | where-object {$_.Name -notlike "*Microsoft*(*.*)*" } | 
+    select  @{n='DriverODBCVer';e={$env:COMPUTERNAME}, Name,Platform,
+        @{n='DriverODBCVer';e={$_.Attribute.DriverODBCVer}},@{n='Driver';e={$_.Attribute.Driver}},
+        @{n='DriverFileVer';e={[string] ((Get-Item $_.Attribute.Driver -ErrorAction SilentlyContinue).VersionInfo.FileVersion)}} | ft
 
 
 write-host "******************** List ODBC DSNs ****************************************"
-Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform'}  | foreach {
-    $attr = $_ | select -ExpandProperty Attribute
-    $_ | select Name, DsnType, Platform, DriverName | select * | ft
-    $attr
+#Get-ODBCDSN |  select Name, DsnType, Platform, DriverName, @{n='Server';e={$_.Attribute.server}} | ft
+Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform'}  |
+    foreach {
+        $attr = $_ | select -ExpandProperty Attribute
+        #filter out empty property values
+        $attr= $attr.GetEnumerator() | Where-Object { $_.Value}
+
+        #display dsn and non-empty attributes
+        $_ | select Name, DsnType, Platform, DriverName | select * | ft
+        $attr
 }
+
+
 
 #**************************************************************************************************************
 #**************************************************************************************************************
@@ -36,11 +37,25 @@ Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform'}  |
 #**************************************************************************************************************
 #**************************************************************************************************************
 
+#utility function which converts a hastbale object to a string array
+function convert-HashToStringArray([hashtable] $HashTableObject)
+{
+    $stringNonemptyAttributes = @() #format must be an array of strings where each string has a Key=Value format
+
+    #filter out empty property values
+    $htArr = $HashTableObject.GetEnumerator() | Where-Object { $_.Value}
+    $htArr | ForEach-Object { $stringNonemptyAttributes+= "$($_.Key)=$($_.Value)"}
+    return  $stringNonemptyAttributes;
+
+}
+
+#create a remote CIM Session
 #New-CIMSession: https://docs.microsoft.com/en-us/powershell/module/cimcmdlets/new-cimsession?view=powershell-7
 $remoteCIMSession= New-CimSession -ComputerName "****ENTER REMOTE COMPUTERNAME HERE*****"
-Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform'} | Add-OdbcDsn -CimSession $remoteCIMSession
-
-
+$extractedDSNs=Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform'} 
+$extractedDSNs | foreach {
+ Add-OdbcDsn -Name $_.Name -DsnType $_.DsnType -Platform $_.Platform -DriverName $_.DriverName -SetPropertyValue (convert-HashToStringArray $_.Attribute) -CimSession $remoteCIMSession
+}
 
 #**************************************************************************************************************
 #**************************************************************************************************************
@@ -51,6 +66,7 @@ Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform'} | 
 #**************************************************************************************************************
 #**************************************************************************************************************
 
+
 #******************************************************************************************************
 #*********** Part 1: Steps below are to be executed on the source machine ****************************************
 #******************************************************************************************************
@@ -59,20 +75,9 @@ write-host "******************** Exporting ODBC DSNs to an XML file  ***********
 $XmlFilePath = "DSNs.xml"
 Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform'} | select * -ExpandProperty Attribute | Export-CLIXML -Path $XmlFilePath -Depth 10 -Verbose
 
-#/*** DEBUG ONLY***
-#**Deleting DSN's for local testing only 
-# Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform' -and $_.name -like "*dummydsn"} | Remove-OdbcDsn
-#renaming DSN for local testing only
-# ((Get-Content -path $XmlFilePath -Raw) -replace 'uDSN3264','uDSN3264dummydsn') | Set-Content -Path $XmlFilePath
-# ((Get-Content -path $XmlFilePath -Raw) -replace 'uDSN32','uDSN32dummydsn') | Set-Content -Path $XmlFilePath
-# ((Get-Content -path $XmlFilePath -Raw) -replace 'sDSN32','sDSN32dummydsn') | Set-Content -Path $XmlFilePath
-# ((Get-Content -path $XmlFilePath -Raw) -replace 'sDSN64','sDSN64dummydsn') | Set-Content -Path $XmlFilePath
-# Get-OdbcDsn -Platform All | where-object {$_.Platform -ne 'Unknown Platform'} | ft
-#*** DEBUG ONLY ***/
-
 write-host "******************** Updating XML file to ensure correct Platform string ****************************************"
-#Replacing string '32/64-bit' to Workaround for this error if a dsn can be used with both 32 + 63
-#Some DSNs can show up as both 32 + 64 bit, platform = "32/64-bit": https://docs.microsoft.com/en-us/previous-versions/windows/desktop/odbc/dn170537(v=vs.85)
+#User DSNs can show up as both 32 + 64 bit, platform = "32/64-bit": https://docs.microsoft.com/en-us/previous-versions/windows/desktop/odbc/dn170537(v=vs.85)
+# Replacing string '32/64-bit' to '64-bit' to workaround for error:
 # Add-OdbcDsn : Cannot validate argument on parameter 'Platform'.
 # The argument "32/64-bit" does not belong to the set "32-bit,64-bit" specified by the ValidateSet attribute.
 # Supply an argument that is in the set and then try the command again.
@@ -82,41 +87,120 @@ write-host "******************** Updating XML file to ensure correct Platform st
 #******************************************************************************************************
 #*********** Part 2: Steps below are to be executed on the destination (new) machine ****************************************
 #******************************************************************************************************
-#Note: Adding a SYSTEM level DSN is a privileged operation. You must RUNAS administrator, or the following error will be raised:
-#Sample error: Add-OdbcDsn : Access denied: low-privilege user cannot manage an ODBC System DSN.
+#Note: Adding a SYSTEM level DSN is a privileged operation. ***YOU MUST RUNAS ADMINISTRATOR***, or the following error will be raised:
+#    Add-OdbcDsn : Access denied: low-privilege user cannot manage an ODBC System DSN.
 #    To manage an ODBC System DSN, start Windows PowerShell with the "Run as administrator" option.
+
+
+
+#utility function which converts a hastbale object to a string array
+function convert-HashToStringArray([hashtable] $HashTableObject)
+{
+    $stringNonemptyAttributes = @() #format must be an array of strings where each string has a Key=Value format
+
+    #filter out empty property values
+    $htArr = $HashTableObject.GetEnumerator() | Where-Object { $_.Value}
+    $htArr | ForEach-Object { $stringNonemptyAttributes+= "$($_.Key)=$($_.Value)"}
+    return  $stringNonemptyAttributes;
+
+}
 
 write-host "******************** Importing ODBC DSNs from an XML file  ****************************************"
 $XmlFilePath = "DSNs.xml"
 $ImportedDSNs = Import-CLIXML -Path $XmlFilePath
 #Loop through each imported DSN, build attribute string, add the DSN
 
-#retrieve list of attributes used the DSN
-foreach($dsn in $ImportedDSNs)
-{ 
-    $attributes = $dsn.Attribute
-    $nonemptyAttributes=@{}
-    $attributes.Keys | ForEach-Object {
-        if($attributes[$_] -and ($attributes[$_].Length -ge 0))
+$ImportedDSNs | foreach {
+    #DEBUG:
+    $pairedAttributeString=  "DSN=" +$_.Name + "|" + ((convert-HashToStringArray $_.Attribute) -join "|")
+    $pairedAttributeString
+
+    #Part2:OptA
+    Add-OdbcDsn -Name $_.Name -DsnType $_.DsnType -Platform $_.Platform -DriverName $_.DriverName -SetPropertyValue (convert-HashToStringArray $_.Attribute)
+
+    #Part2 OptB (using odbcconf tool)
+    #64-bit system:
+    #   odbcconf.exe configsysdsn "ODBC Driver 17 for SQL Server" "DSN=dsn1|SERVER=xyz.database.windows.net|Trusted_Connection=No|Database=AdventureWorksLT"
+    #32-bit system:
+    #   %windir%\syswow64\configdsn "ODBC Driver 17 for SQL Server" "DSN=dsn1|SERVER=xyz.database.windows.net|Trusted_Connection=No|Database=AdventureWorksLT"
+    #if() ....user dsn....
+    #if($_.Platform -eq '64-bit'){     odbcconf.exe configsysdsn $_.DriverName $pairedAttributeString     }
+    #elseif($_.Platform -eq '64-bit'){     %windir%\syswow64\odbcconf.exe configsysdsn $_.DriverName $pairedAttributeString     }
+ }
+ 
+
+#**************************************************************************************************************
+#**************************************************************************************************************
+#**************************************************************************************************************
+# METHOD 3: Export/Import Registry Key
+#**************************************************************************************************************
+#**************************************************************************************************************
+#**************************************************************************************************************
+#**************************************************************************************************************
+
+
+
+#******************************************************************************************************
+#*********** Part 1: Steps below are to be executed on the source machine ****************************************
+#******************************************************************************************************
+
+#https://docs.microsoft.com/en-us/sql/odbc/reference/install/registry-entries-for-data-sources?view=sql-server-ver15
+
+#64-bit System: HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBC.INI
+#32-bit System: HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBC.INI
+#32/64 User: HKCU:\SOFTWARE\ODBC\ODBC.INI
+
+#export via reg.exe: 
+# regedit /e filename.reg "HKEY_LOCAL_MACHINE\SOFTWARE\ODBC\ODBC.INI"
+
+$dsns = @()
+#extract 64-bit system dsn, 32-bit system dsn, and user dsns
+$rootPaths = @("HKLM:\SOFTWARE\ODBC\ODBC.INI\", "HKLM:SOFTWARE\Wow6432Node\ODBC\ODBC.INI\", "HKCU:\SOFTWARE\ODBC\ODBC.INI")
+foreach($root in $rootPaths)
+{
+   $dsnNames=(Get-Item -path $root).GetSubKeyNames() |Where-Object {$_ -notin ("ODBC Data Sources","dBASE Files","Excel Files","MS Access Database") }
+   foreach($dsnName in $dsnNames){
+        $dsnKey = join-path $root $dsnName
+
+        $propertyNames = get-item  -path $dsnKey | select -ExpandProperty Property
+        
+        $currDSNProperties = @{}
+        $currDSNProperties.Add("PSPath",$dsnKey)
+        foreach ($pn in $propertyNames) 
         {
-            $nonemptyAttributes.Add($_, $attributes[$_])
-            #"Keeping " + $_ + "=" + $attributes[$_]
-         
-        }else
-        {
-            #"skipping " + $_ + "=" + $attributes[$_]
+            $value=Get-ItemProperty -path $dsnKey -Name $pn | select -ExpandProperty $pn
+            $currDSNProperties.Add($pn, $value )
         }
-    }
-
-    #Build string array which contains list of attributes
-    #ref: https://stackoverflow.com/questions/26008148/convert-a-hashtable-to-a-string-of-key-value-pairs
-    #Extract into string key/value pairs required for the Add-OdbcDsn commandlet
-    $stringNonemptyAttributes = @() #format must be an array of strings where each string has a Key=Value format
-    #$stringNonemptyAttributes = ($nonemptyAttributes.GetEnumerator() | % { "$($_.Key)=$($_.Value)" })
-    ($nonemptyAttributes.GetEnumerator() | ForEach-Object { $stringNonemptyAttributes+= "$($_.Key)=$($_.Value)"})
-
-    #DEBUG: $dsn.Name + ": " + ($stringNonemptyAttributes -join " ; ")
-
-   #add the odbc DSN
-   Add-OdbcDsn -Name $dsn.Name -DriverName $dsn.DriverName  -Platform $dsn.Platform -DsnType $dsn.DsnType -SetPropertyValue $stringNonemptyAttributes
+        $dsns += $currDSNProperties
+   }
 }
+$dsns | Export-CliXML "dsn_regkeys.xml"
+
+
+
+
+#******************************************************************************************************
+#*********** Part 2: Steps below are to be executed on the destination (new) machine ****************************************
+#******************************************************************************************************
+$dsnsToImport = Import-Clixml "dsn_regkeys.xml"
+foreach ($dsn in $dsnsToImport)
+{
+    $dsnPath = $dsn["PSPath"]
+    new-item $dsnPath
+    $dsn.Keys | Where-Object {$_ -ne "PSPath"} | foreach{  New-ItemProperty -Path $dsnPath -Name $_ -Value $dsn[$_]}
+}
+
+
+#**************************************************************************************************************
+#**************************************************************************************************************
+#**************************************************************************************************************
+# If permitted by the ODBC driver, set Username + Passwords
+#**************************************************************************************************************
+#**************************************************************************************************************
+#**************************************************************************************************************
+#https://social.msdn.microsoft.com/Forums/sqlserver/en-US/d4eab3b3-6254-4644-a1e4-e6866f7507fd/uid-in-odbcini?forum=sqldataaccess
+#https://docs.microsoft.com/en-us/sql/odbc/reference/syntax/sqldriverconnect-function?view=sql-server-ver15
+#Some drivers support storing username and password, most do not
+#$Credential = Get-Credential
+#New-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\ODBC\ODBC.INI\<DSNName> -PropertyType String -Name UID -Value $Credential.UserName
+#New-ItemProperty -Path HKLM:\SOFTWARE\WOW6432Node\ODBC\ODBC.INI\<DSNName> -PropertyType String -Name PWD -Value $Credential.GetNetworkCredential().password
