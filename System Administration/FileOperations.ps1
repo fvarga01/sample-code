df2feaa09e2dcf353cae9e8e6b7ef2308f975682
Fany Vargas <14260028+fvarga01@users.noreply.github.com>
2020-05-13 21:15:19 -0500

    File Ops RegEx

---
+++ System Administration/FileOperations.ps1
@@ -0,0 +1,8 @@
+﻿#Replace special characters in a filename (such as spaces and parenthesis) with the "-" character
+#Regular expression explanation:
+#[\W]?   one or more special characters
+#[^\.\w] exclude the file extension
+
+
+$path =''
+Get-ChildItem $path -Recurse <# -Filter “*Newsletter*” #>| Rename-Item -NewName {$_.name -replace ‘[\W]?[^\.\w]’, '-' -replace '-\.','.'}
