538000253bab344e59adc55ce99bd876d40b4925
fcv <14260028+fvarga01@users.noreply.github.com>
2020-05-28 23:33:27 -0500

    updated samples

--- SystemAdministration/ConfigureDTC.ps1
+++ SystemAdministration/ConfigureDTC.ps1
@@ -20,16 +20,21 @@ Get-NetFirewallRule |
 #check dtc dynamic ports
 netsh int ipv4 show dynamicport tcp
 #confirm dtc process is listening on port 135
-netstat -bano | select-string  -SimpleMatch "0:6","0:1433","0:135" -Context 0,1  #,"sql""RPC","msdtc","wininit"
+netstat -bano | select-string  -SimpleMatch "0:6","0:1433","0:135" `
+    -Context 0,1  #,"sql""RPC","msdtc","wininit"
                
 #configure Windows firewall
 Set-NetFirewallRule -DisplayGroup "Windows Management Instrumentation (WMI)" -Enabled True
 Set-NetFirewallRule -DisplayGroup "Distributed Transaction Coordinator" -Enabled True
 
-New-NetFirewallRule -Name "contoso-DTC-DynamicPorts" -DisplayName  "contoso-DTC-DynamicPorts" -LocalPort 60000-60022 -Enabled True -Protocol tcp -Direction Inbound
-New-NetFirewallRule -Name "Ocontoso-DTC-DynamicPorts" -DisplayName  "Ocontoso-DTC-DynamicPorts" -LocalPort 60000-60022 -Enabled True -Protocol tcp -Direction Outbound
-New-NetFirewallRule -Name "contoso-SQL" -DisplayName  "contoso-SQL" -LocalPort 1433 -Enabled True -Protocol tcp -Direction Inbound
-New-NetFirewallRule -Name "Ocontoso-SQL" -DisplayName  "Ocontoso-SQL" -LocalPort 1433 -Enabled True -Protocol tcp -Direction Outbound
+New-NetFirewallRule -Name "contoso-DTC-DynamicPorts" -DisplayName  "contoso-DTC-DynamicPorts" `
+    -LocalPort 60000-60022 -Enabled True -Protocol tcp -Direction Inbound
+New-NetFirewallRule -Name "Ocontoso-DTC-DynamicPorts" -DisplayName  "Ocontoso-DTC-DynamicPorts" `
+    -LocalPort 60000-60022 -Enabled True -Protocol tcp -Direction Outbound
+New-NetFirewallRule -Name "contoso-SQL" -DisplayName  "contoso-SQL" -LocalPort 1433 `
+    -Enabled True -Protocol tcp -Direction Inbound
+New-NetFirewallRule -Name "Ocontoso-SQL" -DisplayName  "Ocontoso-SQL" -LocalPort 1433 `
+    -Enabled True -Protocol tcp -Direction Outbound
 
 #Check firewall
 Get-NetFirewallRule |
@@ -43,9 +48,12 @@ Get-NetFirewallRule |
 #----------------------------------------------------
 # DTC Security --------------------------------------
 #----------------------------------------------------
-Get-DtcNetworkSetting  | ft –autosize
-Set-DtcNetworkSetting -AuthenticationLevel NoAuth -DtcName "Local" -InboundTransactionsEnabled $True -LUTransactionsEnabled $True -OutboundTransactionsEnabled $True -RemoteAdministrationAccessEnabled $False -RemoteClientAccessEnabled $False -XATransactionsEnabled $True
-Get-DtcNetworkSetting  | ft –autosize
+Get-DtcNetworkSetting  | Format-Table -AutoSize
+Set-DtcNetworkSetting -AuthenticationLevel NoAuth -DtcName "Local" `
+    -InboundTransactionsEnabled $True -LUTransactionsEnabled $True `
+    -OutboundTransactionsEnabled $True -RemoteAdministrationAccessEnabled $False `
+    -RemoteClientAccessEnabled $False -XATransactionsEnabled $True
+Get-DtcNetworkSetting  | Format-Table -AutoSize
 
 #**** limit dtc ports start->run>dcomcnfg *****
 
@@ -74,7 +82,8 @@ test-dtc -LocalComputerName $env:computername -verbose
 New-NetFirewallRule -Name "testDtc1" -DisplayName "testDtc1" -LocalPort 80 -Enabled True -Protocol tcp
 Get-NetFirewallRule | ?{$_.DisplayName -like "*testDtc1*"} |
     Select-Object * | Format-Table -AutoSize
-test-dtc -LocalComputerName $env:computername -RemoteComputerName "Server1"  -verbose -ResourceManagerPort 80 # 17100 #port 0 by default
+test-dtc -LocalComputerName $env:computername -RemoteComputerName "Server1" `
+    -verbose -ResourceManagerPort 80 # 17100 #port 0 by default
 Remove-NetFirewallRule -Name "testDtc1"
 <# In SQL Server - add linked server
 USE [master]
@@ -84,5 +93,6 @@ EXEC master.dbo.sp_addlinkedsrvlogin @rmtsrvname=N'Server1',@useself=N'False',
     @locallogin=NULL,@rmtuser=N'username',@rmtpassword='########'
 GO
 #>
-Invoke-Sqlcmd -ServerInstance Server1 `
-    -Query "begin distributed tran;select name from [Server1].master.sys.databases;waitfor delay '0:0:30';commit" -QueryTimeout 60
+Invoke-Sqlcmd -ServerInstance Server1 -Query `
+    "begin distributed tran;select name from [Server1].master.sys.databases;waitfor delay '0:0:30';commit" `
+    -QueryTimeout 60
